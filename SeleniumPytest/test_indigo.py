a4fea35ba6bf49b264e1c0112d3fc5381ffb89dd
Shubham Sarkar <ronniecse8@gmail.com>
2022-01-17 20:24:45 +0530

    PY2022_17_01_08_23ISTPM

--- SeleniumPytest/test_indigo.py
+++ SeleniumPytest/test_indigo.py
@@ -1,3 +1,4 @@
+# to run the class it is py.test -k indigo -v -s
 import pytest
 
 from selenium import webdriver
@@ -8,18 +9,23 @@ from selenium.webdriver.common.by import By
 
 
 def test_searchflight():
-    # <-------------------------------- For Chrome Browser --------------------------------------------->
-    s = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=s)
 
-    # <-------------------------------- For Firefox ---------------------------------------------------->
-    # s=Service(GeckoDriverManager().install())
-    # driver = webdriver.Firefox(service=s)
-
-    # <--------------------------------- Maximize Window ------------------------------------------------>
-    driver.maximize_window()
 
     try:
+        # <-------------------------------- For Chrome Browser --------------------------------------------->
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {"profile.default_content_setting_values.notifications": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+        s = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
+        driver.implicitly_wait(6)
+
+        # <-------------------------------- For Firefox ---------------------------------------------------->
+        # firefox_option = webdriver.FirefoxOptions()
+        # prefs = {"profile.default_content_setting_values.notifications" : 2}
+        # s=Service(GeckoDriverManager().install())
+        # driver = webdriver.Firefox(service=s , firefox_option =firefox_option)
+        driver.maximize_window()
         driver.get("https://www.goindigo.in/")
     finally:
         driver.minimize_window()
--- pytestDemo/test_demo1.py
+++ pytestDemo/test_demo1.py
@@ -1,6 +1,7 @@
 # Any Pytest File starts with 'test_' , do not start the python file with anything else
 # write any code within function (Test Method) which starts with 'test' keyword
 # to get the HTML Report ---> py.test -v -s --html=reports.html
+
 import pytest
 
 @pytest.mark.xfail
